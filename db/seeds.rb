5da3c1cd6f488ff9970088c1317b867b2ad648f1
Philipp Schiessler <philipp.schiessler@mail.de>
2020-12-20 21:12:16 +0100

    event card setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "clearing data"
+Event.destroy_all
+
+puts "generating events"
+event1 = Event.create!(title: "This is the first event", date_start: Date.parse("03.12.2020"), date_end: Date.parse("03.02.2021"), description: "This is the description of the first event")
+event1.photo.attach(io: File.open(Rails.root.join('app','assets','images','event111.jpg')), filename: 'event111.jpg', content_type: 'image/jpg')
+event2 = Event.create!(title: "This is the second event", date_start: Date.parse("03.04.2021"), date_end: Date.parse("12.05.2021"), description: "This is the description of the second event")
+event2.photo.attach(io: File.open(Rails.root.join('app','assets','images','event222.jpg')), filename: 'event222.jpg', content_type: 'image/jpg')
+event3 = Event.create!(title: "This is the third event", date_start: Date.parse("11.04.2021"), date_end: Date.parse("15.06.2021"), description: "This is the description of the third event")
+event3.photo.attach(io: File.open(Rails.root.join('app','assets','images','event333.jpg')), filename: 'event333.jpg', content_type: 'image/jpg')
+puts "done"
