bbb7a2be092c7f4f4cdcff2230ca6742791df9f0
Philipp Schiessler <philipp.schiessler@mail.de>
2021-01-23 17:47:03 +0100

    cleared JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,14 +24,12 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import { parallaxScroll } from "../plugins/parallax";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  parallaxScroll();
 });
 
 $.fn.blink = function (options) {
